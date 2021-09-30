e64e88a939919c5b17b2e6422200befd3a8cbafa
TheValkDokk <91455763+TheValkDokk@users.noreply.github.com>
2021-10-01 00:48:26 +0700

    update Testing Method

---
+++ src/scripts/validate.js
@@ -0,0 +1,29 @@
+function validate(input) {
+    const validLength = input.length >= 8  //check input length
+    //Validate Logic
+    //Short version but may hard to understand
+
+    let hasLetter = /[a-zA-Z]/g.test(input) //Logic for validate input have Letter
+    let hasNumber = /[0-9]/g.test(input) //Logic for validate input have Number
+
+    //Long version but easier to understand
+
+    // let hasLetter = false;
+    // const A2Z = "abcdefghijklmnopqrstuvwxyz" //from A to Z
+    // for (const letter of A2Z) {
+    //     if (input.toLowerCase().includes(letter)) {
+    //         hasLetter = true
+    //     }
+    // }
+    
+    // let hasNumber = false;
+    // const zero2nine = "0123456789" //from 0 to 9
+    // for (const number of zero2nine) {
+    //     if (input.includes(number)) {
+    //         hasNumber = true
+    //     }
+    // }
+
+    return hasNumber && hasLetter && validLength
+}
+module.exports = validate
---
+++ src/scripts/validate.test.js
@@ -0,0 +1,29 @@
+const validate = require('./validate')
+
+test("returns false for empty input", () => {
+  expect(validate("")).toBe(false)
+})
+
+test("returns false for input without numbers", () => {
+  expect(validate("aksjgkaasdf")).toBe(false)
+})
+
+test("returns false for input without letters", () => {
+  expect(validate("1251234563246")).toBe(false)
+})
+
+test("returns true for input with numbers, letters, >= 8 characters", () => {
+  expect(validate("12512ajskdhgk")).toBe(true)
+})
+
+test("returns false for input with numbers, letters, < 8 characters", () => {
+  expect(validate("a1")).toBe(false)
+})
+
+test("returns true for input with numbers, uppercase letters, and >= 8 characters", () => {
+  expect(validate("12512ASDFA")).toBe(true)
+})
+
+test("returns true for input with numbers, uppercase and lowercase letters, and >= 8 characters", () => {
+  expect(validate("12512ASDasdfasd")).toBe(true)
+})
