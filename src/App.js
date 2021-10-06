c081559e154f9410a23e6dca8d1aee244fb55132
TheValkDokk <91455763+TheValkDokk@users.noreply.github.com>
2021-10-06 13:06:15 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import TextInput from './components/TextInput';
-import DoMath from './components/FarenheitConvert'
+import DoMath from './components/FahrenheitConvert'
 
 function App() {
   return (
